0a7d31e7e1c6ce9d6d3015bf3b4a9ab5e2e85140
Federico Campero <89789363+frcampero@users.noreply.github.com>
2025-04-28 23:48:38 -0300

    Fix email address UPDATE Function

--- config/inngest.js
+++ config/inngest.js
@@ -40,7 +40,7 @@ export const syncUserUpdation = inngest.createFunction(
       event.data;
     const userData = {
       _id: id,
-      email: email_addresses[0].email_addresses,
+      email: email_addresses[0].email_address,
       name: first_name + " " + last_name,
       imageUrl: image_url,
     };
